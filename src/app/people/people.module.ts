486754a98463a693b9f764c59ca21106c3ab3a59
JumanaShireen26 <85886692+JumanaShireen26@users.noreply.github.com>
2021-07-08 08:58:41 -0300

    commit4

--- src/app/people/people.module.ts
+++ src/app/people/people.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from "../angular-material.module";
 import { HttpClientModule } from "@angular/common/http";
-
 import { RouterModule } from "@angular/router";
-
 import { PeopleListComponent } from "./people-list/people-list.component";
 import { PeopleDetailsComponent } from "./people-details/people-details.component";
 import { PeopleRoutingModule } from "./people-routing.module";
